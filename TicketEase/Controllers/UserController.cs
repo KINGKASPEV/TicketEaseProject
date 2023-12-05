8fd6dd6194eb28dfc4f2cc362d3e02b60e97bcae
Kingkaspev <kingsleychiboy22@gmail.com>
2023-12-05 22:45:07 +0100

    UpdateUser Endpoints in UserController

--- TicketEase/Controllers/UserController.cs
+++ TicketEase/Controllers/UserController.cs
@@ -46,5 +46,19 @@ namespace TicketEase.Controllers
 
             return StatusCode(response.StatusCode, new { errors = response.Errors });
         }
+
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> UpdateUser(string id, [FromBody] UpdateUserDto updateUserDto)
+        {
+            var updateResult = await _userServices.UpdateUserAsync(id, updateUserDto);
+
+            if (updateResult.Succeeded)
+            {
+                return Ok(new ApiResponse<bool>(true, "User updated successfully.", 200, true, null));
+            }
+
+            _logger.LogError("User update failed: {Message}", updateResult.Message);
+            return BadRequest(new ApiResponse<bool>(false, "Failed to update user.", 400, false, updateResult.Errors));
+        }
     }
 }
