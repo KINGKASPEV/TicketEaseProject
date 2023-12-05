de7f4af6a1b85c95c09c42c44ef5552f8060eab2
Kingkaspev <kingsleychiboy22@gmail.com>
2023-12-05 18:18:02 +0100

    Models, Entity and General Repository

---
+++ TicketEase.Application/Interfaces/Services/IBoardServices.cs
@@ -0,0 +1,16 @@
+﻿using TicketEase.Application.DTO;
+using TicketEase.Domain;
+using TicketEase.Domain.Entities;
+
+namespace TicketEase.Application.Interfaces.Services
+{
+    public interface IBoardServices
+    {
+        Task<ApiResponse<BoardResponseDto>> AddBoardAsync(BoardRequestDto boardRequestDto);
+        Task<ApiResponse<BoardResponseDto>> UpdateBoardAsync(string boardId, BoardRequestDto boardRequestDto);
+        Task<ApiResponse<GetBoardsDto>> GetAllBoardsAsync(int PerPage, int Page);
+        Task<ApiResponse<BoardResponseDto>> GetBoardByIdAsync(string id);
+        ApiResponse<BoardResponseDto> DeleteAllBoards();
+        Task<ApiResponse<BoardResponseDto>> DeleteBoardAsync(string boardId);
+    }
+}
