3ec862e1bbac3d6f1a05a6a55eb54ff2ae4b6ae6
Kingkaspev <kingsleychiboy22@gmail.com>
2023-12-05 20:40:21 +0100

    GetUserById Endpoints in UserController

--- TicketEase/Controllers/UserController.cs
+++ TicketEase/Controllers/UserController.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketEase.Application.DTO;
+using TicketEase.Application.DTO.Project;
+using TicketEase.Application.Interfaces.Services;
+using TicketEase.Domain;
+
+namespace TicketEase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Route("api/users")]
+
+    public class UserController : ControllerBase
+    {
+        private readonly IUserServices _userServices;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IUserServices userServices, ILogger<UserController> logger)
+        {
+            _userServices = userServices;
+            _logger = logger;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetUserById(string userId)
+        {
+            var response = await _userServices.GetUserByIdAsync(userId);
+
+            if (response.Succeeded)
+            {
+                return Ok(response.Data);
+            }
+
+            return StatusCode(response.StatusCode, new { errors = response.Errors });
+        }
+    }
+}
