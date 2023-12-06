b9e9fe6aa99e92b739bcbd87d4858a548ea78722
Kingkaspev <kingsleychiboy22@gmail.com>
2023-12-06 12:40:35 +0100

    AddTicket Endpoints in TicketController

--- TicketEase/Controllers/TicketController.cs
+++ TicketEase/Controllers/TicketController.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketEase.Application.DTO;
+using TicketEase.Application.Interfaces.Services;
+using TicketEase.Domain;
+using TicketEase.Domain.Enums;
+
+namespace TicketEase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _ticketService;
+
+        public TicketController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpPost("add-ticket")]
+        public IActionResult AddTicket(string userId, string projectId, [FromBody] TicketRequestDto ticketRequestDTO)
+        {
+            var response = _ticketService.AddTicket(userId, projectId, ticketRequestDTO);
+            if (response.Succeeded)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+    }
+}
