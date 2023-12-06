70d8cd68772a3a5267953dfb60982a34dcbc2357
Kingkaspev <kingsleychiboy22@gmail.com>
2023-12-06 14:01:19 +0100

    ProjectController

--- TicketEase/Controllers/ProjectController.cs
+++ TicketEase/Controllers/ProjectController.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketEase.Application.DTO.Project;
+using TicketEase.Application.Interfaces.Services;
+using TicketEase.Common.Utilities;
+using TicketEase.Domain.Entities;
+
+namespace TicketEase.Controllers
+{
+    [Route("Project")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectServices _projectServices;
+        public ProjectController(IProjectServices projectServices)
+        {
+            _projectServices = projectServices;
+        }
+
+        [HttpPost("AddProject/{boardId}")]
+        public async Task<IActionResult> CreateProject(string boardId, [FromBody] ProjectRequestDto project)
+        {
+            return Ok(_projectServices.CreateProjectAsync(boardId, project));
+        }
+    }
+}
