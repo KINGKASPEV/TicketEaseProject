0db46d1dc86e324fb206000cf278b5d44f1915ed
Kingkaspev <kingsleychiboy22@gmail.com>
2023-12-07 14:25:41 +0100

    BoardController

--- TicketEase/Controllers/BoardController.cs
+++ TicketEase/Controllers/BoardController.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TicketEase.Application.DTO;
+using TicketEase.Application.Interfaces.Services;
+using TicketEase.Application.ServicesImplementation;
+using TicketEase.Domain;
+
+namespace TicketEase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardController : ControllerBase
+    {
+        private readonly IBoardServices _boardServices;
+        public BoardController(IBoardServices boardServices)
+        {
+            _boardServices = boardServices;
+        }
+
+        //[Authorize(Roles = "Admin, Manager")]
+        [HttpPost("AddBoard")]
+        public async Task<IActionResult> AddBoard([FromBody] BoardRequestDto request)
+            => Ok(await _boardServices.AddBoardAsync(request));
+    }
+}
