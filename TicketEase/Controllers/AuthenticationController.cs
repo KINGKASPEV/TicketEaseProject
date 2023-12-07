645d72199af67144f31dba307aeb9b0478dac01a
Kingkaspev <kingsleychiboy22@gmail.com>
2023-12-07 22:09:04 +0100

    Authentication Controller

--- TicketEase/Controllers/AuthenticationController.cs
+++ TicketEase/Controllers/AuthenticationController.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TicketEase.Application.DTO;
+using TicketEase.Domain;
+using TicketEase.Domain.Entities;
+
+namespace TicketEase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly Application.Interfaces.Services.IAuthenticationService _authenticationService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AuthenticationController(Application.Interfaces.Services.IAuthenticationService authenticationService, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _authenticationService = authenticationService;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] AppUserCreateDto appUserCreateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ApiResponse<string>(false, "Invalid model state.", StatusCodes.Status400BadRequest, ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList()));
+            }
+            return Ok(await _authenticationService.RegisterAsync(appUserCreateDto));
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(AppUserLoginDto loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ApiResponse<string>(false, "Invalid model state.", 400, ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList()));
+            }
+            return Ok(await _authenticationService.LoginAsync(loginDTO));
+        }
+    }
+}
